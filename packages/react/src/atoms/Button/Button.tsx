f7fda4318b004b01d7e3fd2f72a16b7eaeb005a5
Paul <paulglujan@gmail.com>
2022-11-02 16:40:28 -0700

    Added React and a button component

---
+++ packages/react/src/atoms/Button/Button.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from 'react';
+
+interface ButtonProps {
+  label: string
+}
+
+const Button: FunctionComponent<ButtonProps> = ({ label }) => {
+  return <button>{label}</button>
+};
+
+export default Button;
