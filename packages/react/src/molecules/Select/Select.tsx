44169e9c3a8ec9e38414519973a0c689d3e230b5
Paul <paulglujan@gmail.com>
2022-11-10 09:44:09 -0800

    Cart animation

--- packages/react/src/molecules/Select/Select.tsx
+++ packages/react/src/molecules/Select/Select.tsx
@@ -52,7 +52,14 @@ const Select: React.FC<SelectProps> = ({
       <button className='dse-select__label' onClick={onLabelClick} ref={labelRef}>
         <Text>{selectedOption ? selectedOption.label : label}</Text>
 
-        <svg width={'1rem'} height={'1rem'} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+        <svg
+          className={`dse-select__caret ${isOpen ? 'dse-select__caret--open' : 'dse-select__caret--closed'}`}
+          width={'1rem'} height={'1rem'} xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+        >
           <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
         </svg>
 
