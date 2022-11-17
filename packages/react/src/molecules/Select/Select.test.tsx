3e791050ebdad66060520da8711a142c8bb1b2a0
Paul <paulglujan@gmail.com>
2022-11-17 11:51:56 -0800

    --amend

--- packages/react/src/molecules/Select/Select.test.tsx
+++ packages/react/src/molecules/Select/Select.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Select from "./Select";
 
-import { fireEvent, getByTestId, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 
 interface RenderOptionProps {
   getOptionRecommendedProps: (overrideProps?: Object) => Object
