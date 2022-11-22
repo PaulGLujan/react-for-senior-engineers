aaf0ed3d89d1fb6951c959ecea4aeab924f93eb1
Paul <paulglujan@gmail.com>
2022-11-22 14:53:58 -0800

    chore(build.js): changed CSS output style to compressed
    Also rearranged code to make it more readable

--- packages/scss/src/scripts/build.js
+++ packages/scss/src/scripts/build.js
@@ -2,6 +2,29 @@ const Fs = require("fs");
 const Path = require("path");
 const Sass = require("node-sass");
 
+const main = () => {
+  compile("src/global.scss", "lib/global.css");
+
+  getComponents().forEach((component) => {
+    compile(component.input, component.output);
+  });
+};
+
+const compile = (path, fileName) => {
+  const result = Sass.renderSync({
+    data: Fs.readFileSync(Path.resolve(path)).toString(),
+    outputStyle: "compressed",
+    outFile: "global.css",
+    includePaths: [Path.resolve("src")],
+  });
+
+  try {
+    Fs.mkdirSync(Path.resolve("lib"));
+  } catch (error) {}
+
+  Fs.writeFileSync(Path.resolve(fileName), result.css.toString());
+};
+
 const getComponents = () => {
   const types = ["atoms", "molecules"];
 
@@ -16,23 +39,4 @@ const getComponents = () => {
   return allComponents;
 };
 
-const compile = (path, fileName) => {
-  const result = Sass.renderSync({
-    data: Fs.readFileSync(Path.resolve(path)).toString(),
-    outputStyle: "expanded",
-    outFile: "global.css",
-    includePaths: [Path.resolve("src")],
-  });
-
-  try {
-    Fs.mkdirSync(Path.resolve("lib"));
-  } catch (error) {}
-
-  Fs.writeFileSync(Path.resolve(fileName), result.css.toString());
-};
-
-compile("src/global.scss", "lib/global.css");
-
-getComponents().forEach((component) => {
-  compile(component.input, component.output);
-});
+main();
